d35896c099daff984a599a0dc4be9f3e370b42ee
Osama <osamamurshid@yahoo.com>
2018-05-24 00:53:56 -0400

    CPM Algorithim created Note this assumes the task will have a project refrence and the project will have a list of tasks

--- app/src/main/java/com/brewble/pocketcpmplus/model/project/Project.kt
+++ app/src/main/java/com/brewble/pocketcpmplus/model/project/Project.kt
@@ -2,11 +2,20 @@ package com.brewble.pocketcpmplus.model.project
 
 import com.brewble.pocketcpmplus.model.ListItem
 import com.brewble.pocketcpmplus.model.task.Task
-import java.time.LocalDate
 import java.util.*
 
 data class Project(val startDate: String, override val name: String, override val id: UUID = UUID.randomUUID()): ListItem(){
+    lateinit var taskList: ArrayList<Task>
     fun isValid(): Boolean {
         return !name.isEmpty()
+    }
+    fun getTOC(): Int{
+        var max= 0
+        for(task in taskList){
+            if(max<task.getEF()) {
+                max = task.getEF()
+            }
+        }
+        return max
     }
 }
--- app/src/main/java/com/brewble/pocketcpmplus/model/task/Task.kt
+++ app/src/main/java/com/brewble/pocketcpmplus/model/task/Task.kt
@@ -1,6 +1,43 @@
 package com.brewble.pocketcpmplus.model.task
 
+import com.brewble.pocketcpmplus.model.CPM
 import com.brewble.pocketcpmplus.model.ListItem
+import com.brewble.pocketcpmplus.model.project.Project
 import java.util.*
 
-data class Task(override val name: String, override val id: UUID = UUID.randomUUID(), val startDate: String, val endDate: Date): ListItem()
+data class Task(override val name: String, override val id: UUID = UUID.randomUUID(), val startDate: String, val endDate: Date): ListItem(){
+    private var ES: Int=0
+    private var EF: Int=0
+    private var LS: Int=0
+    private var LF: Int=0
+    val mCPM= CPM(this)
+    private var duration: Int=0
+    lateinit var  project: Project
+    private var depends= ArrayList<Task>()
+    private var preds= ArrayList<Task>()
+    fun getDepends():ArrayList<Task>{
+        return depends
+    }
+    fun getPreds():ArrayList<Task>{
+        return preds
+    }
+    fun getDuration() :Int{
+        return duration
+    }
+    fun getES() :Int{
+        ES= mCPM.getEarlyStart(this)
+        return ES
+    }
+    fun getEF() :Int{
+        EF= mCPM.getEarlyFinish(this)
+        return EF
+    }
+    fun getLS() :Int{
+        LS= mCPM.getLateStart(this)
+        return LS
+    }
+    fun getLF() :Int{
+        LF= mCPM.getLateFinish(this)
+        return LF
+    }
+}
